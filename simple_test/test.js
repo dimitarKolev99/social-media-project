eb2f08c94d031fa7ed8bffd19505f00ed7664f96
Dimitar Kolev <mitko9966@gmail.com>
2022-03-09 19:47:53 +0100

    working on tests

--- simple_test/test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-let add = (x, y) => {
-  return x + y;
-}
-
-assert.equal(add(5, 4), 9, '5 plus 4 should equal 9');
--- test/controllers/commonJest.js
+++ test/controllers/commonJest.js
@@ -8,7 +8,7 @@ module.exports = {
 }
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
