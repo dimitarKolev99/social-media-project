22e655fa0b1c24f967b6f17ef15f4527fe3d1d38
Dimitar Kolev <mitko9966@gmail.com>
2022-03-15 12:26:45 +0100

    added post form on feed

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('btn').addEventListener('click', function() {
+    let form = document.getElementById('postForm');
+    form.submit();
+});
+
+
+function do2(){
+    let imgInput = document.getElementById("imageFile");
+    imgInput.click();
+}
+
+
+ 
