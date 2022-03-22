2e8a512400aea8557ebf4b624e1fd86c0ea2be72
Dimitar Kolev <mitko9966@gmail.com>
2022-03-22 08:45:21 +0100

    fixed a bug where previewing uploaded image was sending multiple requests

--- public/js/script.js
+++ public/js/script.js
@@ -4,25 +4,14 @@ document.getElementById('btn').addEventListener('click', function () {
     form.submit();
 });
 
+var count = 0;
+var imgInput = document.getElementById("imageFile");
 
-async function doRequest() {
-    let imgInput = document.getElementById("imageFile");
+function doRequest() {
     let formData = new FormData();
-    formData.append(imgInput.name, imgInput.files[0], imgInput.files[0].name);
-
     
-    let count;
-    for (let pair of formData.entries()) {
-        count++;
-        console.log(count);
-    }
-    
-    if (count > 1) {
-        console.log('YES');
-        formData.delete(imgInput.name);
-        formData.append(imgInput.name, imgInput.files[0], imgInput.files[0].name);
-    } 
-    
+    formData.set(imgInput.name, imgInput.files[0], imgInput.files[0].name);
+        
     let xhttp = new XMLHttpRequest();
 
     xhttp.open('POST', '/uploadpreview', true);
@@ -31,17 +20,25 @@ async function doRequest() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let res = xhttp.response;
+
+            if (xhttp.response ===
+                '{"status":false,"message":"No file uploaded"}') {
+                    createErrorElement();
+            }
             createElement(res);
         }
     }
+}
 
-    xhttp.onload = function () {
-        if (xhttp.status == 200) {
-            // console.log('HI');
-        } else {
-            console.log('status not 200');
-        }
-      };
+function createErrorElement() {
+    if (document.getElementById('dynamic-image')) {
+        document.getElementById('dynamic-image').remove();
+    }
+
+    let tag = document.createElement('p');
+    p.innerHTML = 'Error uploading image';
+    let card = document.getElementById('target');
+    card.append(tag);
 
 }
 
@@ -55,22 +52,20 @@ function createElement(name) {
 
     tag.setAttribute('src', path);
     tag.setAttribute('id', 'dynamic-image');
-    tag.style.width = '300px';
+    tag.style.width = '100%';
     const card = document.getElementById('target');
     card.append(tag);
 }
+
+imgInput.addEventListener('change', function () {
+    if (imgInput.value) {
+        doRequest();
+    }
+
+});
  
 function openWindow() {
-    let imgInput = document.getElementById("imageFile");
     imgInput.click();
-
-    imgInput.addEventListener('change', async function () {
-        if (imgInput.value) {
-            doRequest();
-        }
-
-    });
-    
 }
 
 
