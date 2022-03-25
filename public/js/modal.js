0e6e2f9d7bbf4b1edde8047abc8335f60b7dae89
Dimitar Kolev <mitko9966@gmail.com>
2022-03-25 18:55:15 +0100

    fixed a bug about opening the modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,15 +1,18 @@
 const inputField = document.querySelector('#content');
 const modal = document.querySelector('.post-modal');
-const picBtn = document.querySelector('.x-wrapper');
+const picBtn = document.querySelector('#pic_upl_btn');
+const picBtnX = document.querySelector('#pic_upl_btn-x');
 
 inputField.addEventListener('click', (e) => {
     e.preventDefault();
     
     modal.setAttribute('is-open', 'true');
-    document.querySelector('.textarea').focus();
-
+    setFocus('.textarea');    
 });
 
+function setFocus(target) {
+    document.querySelector(target).focus();
+}
 const closeBtn = document.querySelector('#close');
 closeBtn.addEventListener('click', (e) => {
     modal.setAttribute('is-open', 'false');
@@ -17,9 +20,35 @@ closeBtn.addEventListener('click', (e) => {
     inputField.value = document.querySelector('.textarea').innerHTML; 
 });
 
+
+
 picBtn.addEventListener('click', (e) => {
+
     if (modal.getAttribute('is-open') === 'false') {
         modal.setAttribute('is-open', 'true');
+        setFocus('.textarea');
+    }
+
+    if (document.querySelector('.preview').getAttribute('data-hidden') == 'true') {
+        document.querySelector('.preview').setAttribute('data-hidden', 'false');
+        
+        document.querySelector('.x-wrapper').style.borderRadius = '20px';
+        document.querySelector('.x-wrapper').style.backgroundColor = '#f0f2f5';
+
+    } else if (document.querySelector('.preview').getAttribute('data-hidden') == 'false') {
+        document.querySelector('.preview').setAttribute('data-hidden', 'true');
+
+        document.querySelector('.x-wrapper').style.borderRadius = '';
+        document.querySelector('.x-wrapper').style.backgroundColor = '';
+    }
+});
+
+
+picBtnX.addEventListener('click', (e) => {
+
+    if (modal.getAttribute('is-open') === 'false') {
+        modal.setAttribute('is-open', 'true');
+        setFocus('.textarea');
     }
 
     if (document.querySelector('.preview').getAttribute('data-hidden') == 'true') {
